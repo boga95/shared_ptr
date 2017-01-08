e0894d0082e5bc59b990adbc439e0310f49b3a7f
boga95 <gabor.borsik@gmail.com>
2017-01-08 21:36:49 +0100

    Fix test cases

--- BG_share_ptr/BG_share_ptr/graph.hpp
+++ BG_share_ptr/BG_share_ptr/graph.hpp
@@ -14,26 +14,26 @@ public:
 	Graph() {}
 	Graph(T *object_) : _object(object_) {}
 	Graph(const Graph<T>& graph);
-	~Graph() { delete _object; }
+	~Graph() {}
 
 	void set_ptr1(const shared_ptr<Graph<T>>& ptr1_) { _ptr1 = ptr1_; }
 	void set_ptr2(const shared_ptr<Graph<T>>& ptr2_) { _ptr2 = ptr2_; }
 
-	T* get_object() const { return _object; } 
+	const shared_ptr<T> get_object() const { return _object.get(); } 
 	const shared_ptr<Graph<T>>& get_ptr1() const { return _ptr1; }
 	const shared_ptr<Graph<T>>& get_ptr2() const { return _ptr2; }
 	
 private:
 	shared_ptr<Graph<T>> _ptr1;
 	shared_ptr<Graph<T>> _ptr2;
-	T* _object;
+	shared_ptr<T> _object;
 };
 
 template<class T>
-Graph<T>::Graph(const Graph<T>& graph) :
-	_object(graph.get_object())
+Graph<T>::Graph(const Graph<T>& graph)
 										
 {
+	_object = graph.get_object();
 	_ptr1 = graph.get_ptr1();
 	_ptr2 = graph.get_ptr2();
 }
